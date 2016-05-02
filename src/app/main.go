1c03edef13e1b9a6a5ea506c958d3e8fe11caa72
A. Emre Ünal <aemreunal@gmail.com>
2016-05-02 14:16:40 -0400

    Add test 1.2

--- src/app/main.go
+++ src/app/main.go
@@ -30,6 +30,21 @@ func main() {
 }
 
 /*
+ * Normally distributed random number generator
+ */
+func getNextNormalRandom(upperLimit int) int {
+	mean := upperLimit / 2
+	stdDev := upperLimit / 6
+	var next int
+	for {
+		next = rand.NormFloat64()*stdDev + mean
+		if next >= 0 && next < upperLimit {
+			return int(next)
+		}
+	}
+}
+
+/*
  * Generic wrapper to run test functions
  */
 func runTest(m IMap, numWrites int, numKeys int, testToRun testFunc) {
@@ -50,6 +65,9 @@ func runTest(m IMap, numWrites int, numKeys int, testToRun testFunc) {
 	wg.Wait()
 }
 
+/*
+ * 1.1
+ */
 func concurrentWrites(m IMap, numWrites int, numKeys int) {
 	for i := 0; i < numWrites; i++ {
 		k := rand.Int()
@@ -57,3 +75,21 @@ func concurrentWrites(m IMap, numWrites int, numKeys int) {
 		m.Put(k, v)
 	}
 }
+
+/*
+ * 1.2
+ */
+func concurrentWritesNormalDist(m IMap, numWrites int, numKeys int) {
+	for i := 0; i < numWrites; i++ {
+		k := getNextNormalRandom(numWrites)
+		v := k
+		m.Put(k, v)
+	}
+}
+
+// /*
+//  * 1.2
+//  */
+// func func_name(m IMap, numWrites int, numKeys int) {
+
+// }
