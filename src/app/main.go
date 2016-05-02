ccce6cb01241abd754ca0e7e7fc2dee9220d5211
A. Emre Ünal <aemreunal@gmail.com>
2016-05-02 15:10:03 -0400

    Unexport constants and types

--- src/app/main.go
+++ src/app/main.go
@@ -9,24 +9,24 @@ import (
 )
 
 const (
-	NumWritesInWriteOnlyTestSmall     = 1024 * 1024 * 16 // 16 M
-	NumWritesInRWTestSmall            = 1024 * 1024 * 16 // 16 M
-	NumReadsInReadOnlyTestSmall       = 1024 * 1024 * 16 // 16 M
-	NumIterationInConcurrentReadWrite = 10 * 1024 * 16
-	NumWriteDeleteIter                = 5
-	NumKeysInBigMap                   = 1024 * 1024 * 16 // 16 M
-	NumKeysInSmallMap                 = 1024 * 16
-	WriteRatioHigh                    = 1000
-	WriteRatioLow                     = 2
+	numWritesInWriteOnlyTestSmall     = 1024 * 1024 * 16 // 16 M
+	numWritesInRWTestSmall            = 1024 * 1024 * 16 // 16 M
+	numReadsInReadOnlyTestSmall       = 1024 * 1024 * 16 // 16 M
+	numIterationInConcurrentReadWrite = 10 * 1024 * 16
+	numWriteDeleteIter                = 5
+	numKeysInBigMap                   = 1024 * 1024 * 16 // 16 M
+	numKeysInSmallMap                 = 1024 * 16
+	writeRatioHigh                    = 1000
+	writeRatioLow                     = 2
 )
 
-type IMap interface {
+type iMap interface {
 	Get(k interface{}) (interface{}, bool)
 	Put(k, v interface{}) interface{}
 	Remove(k interface{}) (interface{}, bool)
 }
 
-type testFunc func(IMap, int, int)
+type testFunc func(iMap, int, int)
 
 func main() {
 	// Set num. threads to be equal to num. of logical processors
@@ -34,7 +34,18 @@ func main() {
 
 	// Test concurrentWrites
 	fmt.Println("Testing concurrentWrites")
-	runTest(lockmap.NewLockMap(), NumWritesInWriteOnlyTestSmall, NumWritesInWriteOnlyTestSmall, concurrentWrites)
+	runTest(lockmap.NewLockMap(), numWritesInWriteOnlyTestSmall, numWritesInWriteOnlyTestSmall, concurrentWrites)
+}
+
+/*
+ * Initializes a map
+ */
+func initializeMap(nKeys int, m iMap) {
+	for i := 0; i < nKeys; i++ {
+		k := i
+		v := fmt.Sprintf("%12d", k)
+		m.Put(k, v)
+	}
 }
 
 /*
@@ -55,7 +66,7 @@ func getNextNormalRandom(upperLimit int) int {
 /*
  * Generic wrapper to run test functions
  */
-func runTest(m IMap, numWrites int, numKeys int, testToRun testFunc) {
+func runTest(m iMap, numWrites int, numKeys int, testToRun testFunc) {
 	// Create waitgroup to wait for goroutines to finish
 	var wg sync.WaitGroup
 	wg.Add(runtime.NumCPU())
@@ -76,7 +87,7 @@ func runTest(m IMap, numWrites int, numKeys int, testToRun testFunc) {
 /*
  * 1.1
  */
-func concurrentWrites(m IMap, numWrites int, numKeys int) {
+func concurrentWrites(m iMap, numWrites int, numKeys int) {
 	for i := 0; i < numWrites; i++ {
 		k := rand.Int()
 		v := fmt.Sprintf("%12d", k)
@@ -87,7 +98,7 @@ func concurrentWrites(m IMap, numWrites int, numKeys int) {
 /*
  * 1.2
  */
-func concurrentWritesNormalDist(m IMap, numWrites int, numKeys int) {
+func concurrentWritesNormalDist(m iMap, numWrites int, numKeys int) {
 	for i := 0; i < numWrites; i++ {
 		k := getNextNormalRandom(numWrites)
 		v := fmt.Sprintf("%12d", k)
@@ -98,7 +109,7 @@ func concurrentWritesNormalDist(m IMap, numWrites int, numKeys int) {
 /*
  * 1.3
  */
-func concurrentWritesSequential(m IMap, numWrites int, numKeys int) {
+func concurrentWritesSequential(m iMap, numWrites int, numKeys int) {
 	currentKey := 0
 	for i := 0; i < numWrites; i++ {
 		k := currentKey
@@ -111,9 +122,9 @@ func concurrentWritesSequential(m IMap, numWrites int, numKeys int) {
 /*
  * 2.1
  */
-func lotsWritesFewReads(m IMap, numWrites int, numKeys int) {
+func lotsWritesFewReads(m iMap, numWrites int, numKeys int) {
 	for i := 0; i < numWrites; i++ {
-		if i > 0 && i%WriteRatioHigh == 0 {
+		if i > 0 && i%writeRatioHigh == 0 {
 			/* Do a read */
 			k := rand.Int()
 			v, ok := m.Get(k)
@@ -135,9 +146,9 @@ func lotsWritesFewReads(m IMap, numWrites int, numKeys int) {
 /*
  * 2.2
  */
-func lotsWritesFewReadsNormalDist(m IMap, numWrites int, numKeys int) {
+func lotsWritesFewReadsNormalDist(m iMap, numWrites int, numKeys int) {
 	for i := 0; i < numWrites; i++ {
-		if i > 0 && i%WriteRatioHigh == 0 {
+		if i > 0 && i%writeRatioHigh == 0 {
 			/* Do a read */
 			k := getNextNormalRandom(numWrites)
 			v, ok := m.Get(k)
@@ -159,11 +170,11 @@ func lotsWritesFewReadsNormalDist(m IMap, numWrites int, numKeys int) {
 /*
  * 2.3
  */
-func lotsWritesFewReadsSequential(m IMap, numWrites int, numKeys int) {
+func lotsWritesFewReadsSequential(m iMap, numWrites int, numKeys int) {
 	currentWriteKey := 0
 	currentReadKey := 0
 	for i := 0; i < numWrites; i++ {
-		if i > 0 && i%WriteRatioHigh == 0 {
+		if i > 0 && i%writeRatioHigh == 0 {
 			/* Do a read */
 			k := currentReadKey
 			currentReadKey = (currentReadKey + 1) % numWrites
@@ -187,9 +198,9 @@ func lotsWritesFewReadsSequential(m IMap, numWrites int, numKeys int) {
 /*
  * 3.1
  */
-func lotsWritesLotsReads(m IMap, numWrites int, numKeys int) {
+func lotsWritesLotsReads(m iMap, numWrites int, numKeys int) {
 	for i := 0; i < numWrites; i++ {
-		if i > 0 && i%WriteRatioLow == 0 {
+		if i > 0 && i%writeRatioLow == 0 {
 			/* Do a read */
 			k := rand.Int()
 			v, ok := m.Get(k)
@@ -211,9 +222,9 @@ func lotsWritesLotsReads(m IMap, numWrites int, numKeys int) {
 /*
  * 3.2
  */
-func lotsWritesLotsReadsNormalDist(m IMap, numWrites int, numKeys int) {
+func lotsWritesLotsReadsNormalDist(m iMap, numWrites int, numKeys int) {
 	for i := 0; i < numWrites; i++ {
-		if i > 0 && i%WriteRatioLow == 0 {
+		if i > 0 && i%writeRatioLow == 0 {
 			/* Do a read */
 			k := getNextNormalRandom(numWrites)
 			v, ok := m.Get(k)
@@ -236,7 +247,7 @@ func lotsWritesLotsReadsNormalDist(m IMap, numWrites int, numKeys int) {
 /*
  * 3.3
  */
-func lotsWritesLotsReadsSequential(m IMap, numWrites int, numKeys int) {
+func lotsWritesLotsReadsSequential(m iMap, numWrites int, numKeys int) {
 	currentKey := 0
 	for i := 0; i < numWrites; i++ {
 		/* Write if i is even, read if i is odd */
